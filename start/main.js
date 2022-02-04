f06f979ce24825d802c09fe8aa949f65d6ed2d3b
SunMin04 <93808789+SunMin04@users.noreply.github.com>
2022-02-05 00:37:06 +0900

    Add files via upload

---
+++ start/main.js
@@ -0,0 +1,36 @@
+let target = document.querySelector("#dynamic");
+
+function randomString(){
+    let StringArr = ["My name is KimSunMin", "This is my portfolio", "Learn To HTML",
+    "Learn To CSS", "Learn To JavaScript"];
+let selectString = StringArr[Math.floor(Math.random() * StringArr.length)];
+let selectStringArr = selectString.split("");
+
+return selectStringArr;
+}
+
+//타이핑 리셋
+function resetTyping(){
+    target.textContent = "";
+    dynamic(randomString());
+}
+
+//한글자씩 텍스트 출력 함수
+function dynamic(randomArr){
+    if(randomArr.length > 0){
+        target.textContent +=randomArr.shift();
+        setTimeout(function(){
+            dynamic(randomArr);
+        }, 80);
+    }else{
+        setTimeout(resetTyping, 2000);
+    }
+}
+
+dynamic(randomString());
+
+//커서 깜빡임 효과
+function blink(){
+    target.classList.toggle("active");
+}
+setInterval(blink, 500);
